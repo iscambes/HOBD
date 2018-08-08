acec2e7c23a33b07f446e2eab01521c16a689c36
Tim Vaughan <tgvaughan@gmail.com>
2018-08-08 11:10:12 +0200

    Adding burst events to simulator.

--- src/hobd/simulator/SimulatedHOBDTree.java
+++ src/hobd/simulator/SimulatedHOBDTree.java
@@ -4,6 +4,7 @@ import beast.core.Input;
 import beast.core.parameter.RealParameter;
 import beast.evolution.tree.Node;
 import beast.evolution.tree.Tree;
+import beast.math.Binomial;
 import beast.util.Randomizer;
 import hobd.HOBDModelParams;
 
@@ -58,22 +59,25 @@ public class SimulatedHOBDTree extends Tree {
             HOBDEvent event = eventList.get(eventIdx);
             HOBDState state = stateList.get(eventIdx);
 
-            int k = activeLineages.size();
+            int n = activeLineages.size();
             double I = state.popSize;
             double age = params.getOriginTime() - event.time;
 
+            double pCoal;
+            Node parent;
+
             switch(event.type) {
                 case BIRTH:
-                    double pCoal = k*(k-1.0)/(I*(I-1.0));
+                    pCoal = n*(n-1.0)/(I*(I-1.0));
                     if (Randomizer.nextDouble()>pCoal)
                         break;
 
-                    Node left = activeLineages.get(Randomizer.nextInt(k));
+                    Node left = activeLineages.get(Randomizer.nextInt(n));
                     activeLineages.remove(left);
-                    Node right = activeLineages.get(Randomizer.nextInt(k-1));
+                    Node right = activeLineages.get(Randomizer.nextInt(n-1));
                     activeLineages.remove(right);
 
-                    Node parent = new Node();
+                    parent = new Node();
                     parent.setNr(nextInternalNodeNr++);
                     parent.addChild(left);
                     parent.addChild(right);
@@ -83,6 +87,47 @@ public class SimulatedHOBDTree extends Tree {
                     break;
 
                 case BURST:
+                    if (n<2)
+                        break;
+
+                    double[] p = new double[n-1];
+
+                    int burstSize = event.count;
+
+                    pCoal = 0.0;
+                    for (int k=2; k<=n; k++) {
+                        p[k-2] = Math.exp(Binomial.logChoose(burstSize, k) + Binomial.logChoose(n, k) - Binomial.logChoose((int)Math.round(I), k));
+                        pCoal += p[k-2];
+                    }
+
+                    if (Randomizer.nextDouble()>pCoal)
+                        break;
+
+                    double u = Randomizer.nextDouble()*pCoal;
+                    int k;
+                    for (k=2; k<=n; k++) {
+                        if (u<p[k-2])
+                            break;
+
+                        u -= p[k-2];
+                    }
+
+                    System.out.println("Burst size " + burstSize + " k=" + k);
+
+                    parent = new Node();
+                    parent.setNr(nextInternalNodeNr++);
+                    parent.setHeight(age);
+
+                    for (int i=0; i<k; i++) {
+                        Node child = activeLineages.get(Randomizer.nextInt(n));
+                        activeLineages.remove(child);
+                        n -= 1;
+
+                        parent.addChild(child);
+                    }
+
+                    activeLineages.add(parent);
+                    break;
 
                 case PSISAMPLE:
                 case RHOSAMPLE:
@@ -108,16 +153,21 @@ public class SimulatedHOBDTree extends Tree {
 
     public static void main(String[] args) {
 
-        Randomizer.setSeed(15);
+//        for (int s=1; s<100; s++) {
+//            Randomizer.setSeed(s);
+//
+//            System.out.println("** " + s + " **");
+        Randomizer.setSeed(3);
 
-        SimulatedHOBDTree tree = new SimulatedHOBDTree();
+            SimulatedHOBDTree tree = new SimulatedHOBDTree();
 
-        HOBDModelParams params = new HOBDModelParams(1.3, 0.8,
-                0.0, 10, 0.3, 0.1,
-                3.0);
+            HOBDModelParams params = new HOBDModelParams(1.3, 0.8,
+                    0.1, 10, 0.3, 0.1,
+                    3.0);
 
-        tree.initByName("modelParams", params);
+            tree.initByName("modelParams", params);
 
-        System.out.println(tree);
+            System.out.println(tree);
+//        }
     }
 }
