e136b095dd3d32b5cf2792a10dd73729a44499f3
iscambes <israel.campo.bes@gmail.com>
2018-08-08 11:11:06 +0200

    Some changes.

--- src/hobd/HOBDModel.java
+++ src/hobd/HOBDModel.java
@@ -5,6 +5,7 @@ import beast.core.parameter.RealParameter;
 import beast.evolution.speciation.SpeciesTreeDistribution;
 import beast.evolution.tree.Node;
 import beast.evolution.tree.TreeInterface;
+import beast.math.GammaFunction;
 import beast.util.TreeParser;
 import org.apache.commons.math3.exception.DimensionMismatchException;
 import org.apache.commons.math3.exception.MaxCountExceededException;
@@ -12,12 +13,19 @@ import org.apache.commons.math3.ode.AbstractIntegrator;
 import org.apache.commons.math3.ode.FirstOrderDifferentialEquations;
 import org.apache.commons.math3.ode.nonstiff.ClassicalRungeKuttaIntegrator;
 import org.apache.commons.math3.ode.nonstiff.EulerIntegrator;
-import org.apache.commons.math3.ode.nonstiff.RungeKuttaIntegrator;
+import pitchfork.Pitchforks;
 
-public class BirthDeathModelNumeric extends SpeciesTreeDistribution {
+import java.util.List;
+
+
+
+public class HOBDModel extends SpeciesTreeDistribution {
 
     public Input<RealParameter> birthRateInput = new Input<>("birthRate",
             "Per-individual birth rate.", Input.Validate.REQUIRED);
+
+    public Input<RealParameter> superRateInput = new Input<>("superRate",
+            "Per-individual super Rate.", Input.Validate.REQUIRED);
 
     public Input<RealParameter> deathRateInput = new Input<>("deathRate",
             "Per-individual death rate.", Input.Validate.REQUIRED);
@@ -31,32 +39,42 @@ public class BirthDeathModelNumeric extends SpeciesTreeDistribution {
     public Input<RealParameter> timeOriginInput = new Input<>("timeOrigin",
             "Per-individual time Origin.", Input.Validate.REQUIRED);
 
+    public Input<RealParameter> meanInput = new Input<>("mean",
+            "Per-individual mean.", Input.Validate.REQUIRED);
 
 
 
-    RealParameter birthRate, deathRate, samplingRate, presentSamplingProb, timeOrigin;
+
+
+
+
+    RealParameter birthRate, superRate, deathRate, samplingRate, presentSamplingProb, timeOrigin, mean;
 
     @Override
     public void initAndValidate() {
         birthRate = birthRateInput.get();
+        superRate = superRateInput.get();
         deathRate = deathRateInput.get();
         samplingRate = samplingRateInput.get();
         presentSamplingProb = presentSamplingProbInput.get();
         timeOrigin = timeOriginInput.get();
-
-
+        mean = meanInput.get();
 
     }
 
     class ODEp0 implements FirstOrderDifferentialEquations {
 
-        double lambda, mu, psi, rho;
+        double lambda, mu, psi, rho, mean, sup;
 
-        public ODEp0(double lambda, double mu, double psi, double rho) {
+        public ODEp0(double lambda, double mu, double psi, double rho, double mean, double sup) {
             this.lambda = lambda;
             this.mu = mu;
             this.psi = psi;
             this.rho = rho;
+            this.mean = mean;
+            this.sup = sup;
+
+
         }
 
         @Override
@@ -70,16 +88,18 @@ public class BirthDeathModelNumeric extends SpeciesTreeDistribution {
 
             double p0 = y[0];
 
-            yDot[0] = -(lambda + mu + psi)*p0 + mu + lambda*p0*p0;
+            yDot[0] = -(lambda + mu + psi + sup)*p0 + mu + (lambda*p0*p0) + (sup*(Math.exp(mean*(p0-1)))*p0);
+
         }
     }
+
 
     public double get_p0(double t) {
 
         ODEp0 p0equations = new ODEp0(birthRate.getValue(), deathRate.getValue(),
-                samplingRate.getValue(), presentSamplingProb.getValue());
+                samplingRate.getValue(), presentSamplingProb.getValue(), superRate.getValue(), mean.getValue());
 
-        // AbstractIntegrator integrator = new EulerIntegrator(0.001);
+        //AbstractIntegrator integrator = new EulerIntegrator(0.02);
         AbstractIntegrator integrator = new ClassicalRungeKuttaIntegrator(0.001);
 
         double [] state = {1 - presentSamplingProb.getValue()};
@@ -91,21 +111,21 @@ public class BirthDeathModelNumeric extends SpeciesTreeDistribution {
         return state[0];
     }
 
+
     public class ODEgep0 implements FirstOrderDifferentialEquations {
 
-        double lambda, mu, psi, rho;
+        double lambda, mu, psi, rho, mean, sup;
 
-        public ODEgep0(double lambda, double mu, double psi, double rho) {
+        public ODEgep0(double sup, double lambda, double mu, double psi, double rho, double mean) {
             this.lambda = lambda;
             this.mu = mu;
             this.psi = psi;
             this.rho = rho;
+            this.mean = mean;
+            this.sup = sup;
         }
-
-
         @Override
-        public int getDimension() {
-            return 2;
+        public int getDimension() { return 2;
         }
 
         @Override
@@ -114,8 +134,11 @@ public class BirthDeathModelNumeric extends SpeciesTreeDistribution {
             double p0 = y[0];
             double ge = y[1];
 
-            double p0Dot = -(lambda + mu + psi)*p0 + mu + lambda*p0*p0;
-            double geDot = -(lambda + mu + psi)*ge + (2*lambda*p0*ge);
+            ////ECUACIONES DIFERENCIALES.
+
+            double p0Dot = -(lambda + mu + psi + sup)*p0 + mu + (lambda*p0*p0) + (sup* Math.exp(mean *(p0-1))*p0);
+            double geDot = -(lambda + mu + psi + sup)*ge + (2*lambda*p0*ge) + (sup*(mean *p0+1)*Math.exp(mean *(p0-1))*ge);
+
 
 
             yDot[0] = p0Dot;
@@ -125,7 +148,7 @@ public class BirthDeathModelNumeric extends SpeciesTreeDistribution {
 
     double[] get_gep0(double t, Node node) {
         // here we have to implement recursive formula
-        // además, va a estar basado en las BONDER CONDITIONS!
+
 
         double[] state = new double[2];
 
@@ -142,29 +165,72 @@ public class BirthDeathModelNumeric extends SpeciesTreeDistribution {
                 state[1] = samplingRate.getValue();
 
             } else {
+
                 // rho-sampling
 
                 state[0] = 1-presentSamplingProb.getValue();
                 state[1] = presentSamplingProb.getValue();
-
             }
+
         } else {
+
             //internal node
 
-            double[] stateLeft = get_gep0(node.getHeight(), node.getLeft());
-            double[] stateRight = get_gep0(node.getHeight(), node.getRight());
+            List<Node> children = Pitchforks.getLogicalChildren(node);
 
-            state[0] = stateLeft[0]; // p0 at start of edge. we use just one of the p0 , cause are the same!!
-            state[1] = birthRate.getValue() * stateLeft[1] * stateRight[1];
+            int k = children.size();
+
+            if (k==2.0) {
+
+
+                double[] state0 = get_gep0(node.getHeight(), children.get(0));    /// ANTES, EN VEZ DE CHILDREN 0 Y 1 METIAMOS DERECHA E IZQUIERDA. AHORA YA NO VAMOS A UTILIZRA ESO PORQUE ESTAMOS UTILIZADO EL PITCHFORKS.
+                double[] state1 = get_gep0(node.getHeight(), children.get(1));
+
+
+                state[0] = state0[0];
+                state[1] = (birthRate.getValue() + 1) * state0[1] * state1[1];
+
+
+            }else{
+
+                // aqui hemos creado un nuevo array. dice que calcular todos los gep0 de cada hijo (childNode) de todos los hijos (children).
+                // aqui vamos a obtener un array con los diferentes gep0 para todos los nodos.
+
+                double p0init = 0.0;
+                double geinit = 0.0;
+
+                for (int n=0; n<=k-2; n+=1) {
+                    geinit += Math.exp(-mean.getValue() + n*Math.log(mean.getValue()) - GammaFunction.lnGamma(1+n));
+                }
+
+                geinit = 1.0 - geinit;
+
+                // esto que aparece ahora aqui abajo es para calcular p0. queremos simplemente calcular el p0 del primero, pues
+                //la altura del nodo es igual para todas las edges
+
+                boolean isFirst = true;
+                for (Node childNode : children) {
+                    double[] childState = get_gep0(node.getHeight(), childNode);
+
+                    geinit *= childState[1];
+
+                    if (isFirst) {
+                        p0init = childState[0];
+                        isFirst = false;
+                    }
+                }
+
+                state[0] = p0init;
+                state[1] = geinit;
+            }
         }
 
 
-
-        ODEgep0 gep0equations = new ODEgep0(birthRate.getValue(), deathRate.getValue(),
-                samplingRate.getValue(), presentSamplingProb.getValue());
+            ODEgep0 gep0equations = new ODEgep0(birthRate.getValue(), deathRate.getValue(),
+                    samplingRate.getValue(), presentSamplingProb.getValue(),superRate.getValue(), mean.getValue());
 
         // AbstractIntegrator integrator = new EulerIntegrator(0.001);
-        AbstractIntegrator integrator = new ClassicalRungeKuttaIntegrator(0.001);
+        AbstractIntegrator integrator = new ClassicalRungeKuttaIntegrator(0.1);
 
         // Integrate ge and p0 along edge
         integrator.integrate(gep0equations, node.getHeight(), state, t, state);
@@ -177,6 +243,9 @@ public class BirthDeathModelNumeric extends SpeciesTreeDistribution {
         return get_gep0(t, node)[1];
     }
 
+
+
+
     @Override
     public double calculateTreeLogLikelihood(TreeInterface tree) {
         double logP = 0.0;
@@ -186,12 +255,31 @@ public class BirthDeathModelNumeric extends SpeciesTreeDistribution {
         return logP;
     }
 
+
     public static void main(String[] args) {
 
-        double b=1.0, d = 0.5, s = 0.6, rho = 0.2, t0 = 5.0;
+        double b=1.0, d = 0.5, s = 0.6, rho = 0.2, t0 = 5.0, m = 20, j = 10;
 
-        TreeParser tree = new TreeParser("(A:1.0,B:1.0):1.0;");
+        TreeParser tree = new TreeParser("(A:1.0,B:1.0):1.0;");   // con sto del tree pasrser podiamos ir analizando como de uno en un edge. la parte de arriba es para varios (dos creo) y lo de abajo que esta con // para UNO SOLO
+
 //        TreeParser tree = new TreeParser("A:1.0;");
+
+        //SUPERSPREADING MODEL
+
+        HOBDModel HOBDModel = new HOBDModel();
+        HOBDModel.initByName("birthRate", new RealParameter(String.valueOf(b)),
+                "deathRate", new RealParameter(String.valueOf(d)),
+                "samplingRate", new RealParameter(String.valueOf(s)),
+                "presentSamplingProb", new RealParameter(String.valueOf(rho)),
+                "timeOrigin", new RealParameter(String.valueOf(t0)),
+                "mean", new RealParameter(String.valueOf(m)),
+                "superRate", new RealParameter(String.valueOf(j)),
+                "tree", tree);
+
+        double p0HOBDModel = HOBDModel.get_p0(t0);
+        double geHOBDMODEL = HOBDModel.get_ge(t0, tree.getRoot());
+
+        //NUMERICALMODEL
 
         BirthDeathModelNumeric bdmodelNumeric = new BirthDeathModelNumeric();
         bdmodelNumeric.initByName("birthRate", new RealParameter(String.valueOf(b)),
@@ -203,6 +291,8 @@ public class BirthDeathModelNumeric extends SpeciesTreeDistribution {
 
         double p0numeric = bdmodelNumeric.get_p0(t0);
         double geNumeric = bdmodelNumeric.get_ge(t0, tree.getRoot());
+
+        //ANALYTIC MODEL
 
         BirthDeathModelAnalytic bdmodelAnalytic = new BirthDeathModelAnalytic();
         bdmodelAnalytic.initByName("birthRate", new RealParameter(String.valueOf(b)),
@@ -218,9 +308,10 @@ public class BirthDeathModelNumeric extends SpeciesTreeDistribution {
         double p0analytic = bdmodelAnalytic.get_p0(t0, b, d, s, c1, c2);
         double geAnalytic = Math.exp(bdmodelAnalytic.calculateTreeLogLikelihood(tree));
 
-        System.out.println("p0: " + p0analytic + "\t" + p0numeric);
-        System.out.println("ge: " + geAnalytic + "\t" + geNumeric);
 
+
+        System.out.println("p0: " + p0analytic + "\t" + p0HOBDModel + "\t" + p0numeric);
+        System.out.println("ge: " + geAnalytic + "\t" + geHOBDMODEL + "\t" + geNumeric);
 
 
     }
